c035ac833814e8cf3b5564c165f7838bbbaabcf8
wangchaosun <2962056732@qq.com>
2024-07-10 15:44:40 +0800

    simpleclient add cmdParser

--- ref/simpleclient/simpleclient.c
+++ ref/simpleclient/simpleclient.c
@@ -49,7 +49,6 @@ void*redisRecvDaemon(void * arg){
 		FD_SET(fd,&read_set);
 	    timeout.tv_sec = 5;
 	    timeout.tv_usec= 0;
-		printf("working...\r\n");
 		rv = select(fd+1,&read_set,NULL,NULL,&timeout);
 		if( rv <= 0 ) continue;
         // recv and print 
@@ -61,9 +60,81 @@ void*redisRecvDaemon(void * arg){
     }
 }
 
-int simpleParser(char* raw_cmd, char*cmd){
+int startWith(const char * origin, const char * prefix) {
+	if( origin == NULL || prefix == NULL || 
+        strlen(origin) < strlen(prefix))    return 0;
 	
-	// return 0;
+	for( int i=0;i<strlen(prefix) ;i++ ) {
+		if( origin[i] != prefix[i]) return 0;
+	}
+	return 1;
+}
+
+int countWord(const char* cmd) {
+	int32_t count = 0;
+	uint8_t word_flag = 0;
+	for( int i=0;i<strlen(cmd); i++ ) {
+		if( cmd[i] != ' ' && word_flag == 0 ) {
+			word_flag = 1;
+		}
+		
+		if( cmd[i] == ' '  && word_flag == 1 ) {
+			word_flag = 0;
+			count ++;
+		}
+	}
+	return count+1;// the last one word is missing
+}
+
+int getOneWord(char * cmd,int * offset) {
+	int count = 0;
+	int word_flag = 0;
+	(*offset ) = 0;
+	for( int i=0;i<strlen(cmd)+1;i++ ){
+		if( cmd[i] == ' ' && word_flag == 0 ){
+			(*offset) ++;
+		}else if( cmd[i] != ' '&& word_flag == 0) {
+			word_flag = 1;
+			count ++;
+		}else if( (cmd[i] != ' ')&& (cmd[i]!= '\0')
+                  && word_flag == 1) {
+			count ++;
+		}else if((cmd[i] == ' ' || cmd[i] == 0)
+                  && word_flag == 1) {
+			word_flag = 0;
+			return count;
+		}else{}
+	}
+	return 0;
+}
+
+int simpleParser(char* raw_cmd, char*cmd){
+	if( strcmp(raw_cmd,"PING" )== 0
+        ||strcmp(raw_cmd,"ping" )== 0 ) {
+		strcpy(cmd,"PING\r\n");
+		return 0;
+	}
+    if( strlen( raw_cmd ) >= 5 ) {  // "set x" or "get x" must longer than 5
+    	// simple set/get cmd
+		int count = 0;
+		int offset = 0;
+		int word_len = 0;
+       	if( startWith(raw_cmd,"set ")|| startWith(raw_cmd,"get ")) {
+	        count = countWord(raw_cmd);
+			sprintf(cmd,"*%d\r\n",count);
+			cmd = cmd + strlen(cmd);
+			for( int j=0;j<count;j++ ) {
+				word_len = getOneWord(raw_cmd,&offset);
+				raw_cmd = raw_cmd + offset;
+				raw_cmd[word_len] = '\0';
+				sprintf(cmd,"$%d\r\n%s\r\n",word_len,raw_cmd);
+				raw_cmd = raw_cmd + word_len + 1;
+				cmd = cmd + strlen(cmd);
+			}
+			return 0;
+		}
+	}
+	
 	return -1;
 }
 int main(){
@@ -82,7 +153,15 @@ int main(){
 	char  cmd_buf[1024];
     int   cmd_len ;
     while( 1 ) {
-        scanf("%s",raw_cmd_buf);
+        // scanf("%s",raw_cmd_buf);
+		fgets(raw_cmd_buf,sizeof(raw_cmd_buf),stdin);
+		// remove newline flag \n
+		if( raw_cmd_buf[strlen(raw_cmd_buf)-2] == '\r') {
+			raw_cmd_buf[strlen(raw_cmd_buf)-2] = '\0';
+		}
+		if( raw_cmd_buf[strlen(raw_cmd_buf)-1] == '\n') {
+			raw_cmd_buf[strlen(raw_cmd_buf)-1] = '\0';
+		}
         if( strcmp(raw_cmd_buf,"exit") == 0) break;
 		// append \r\n
 	    // cmd_len = strlen(cmd_buf);
